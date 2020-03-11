408df3e9d76d0f09803e9bed1d54815522832ae3
codemanfix <codemanfix@mail.ru>
2020-03-11 19:33:37 +0400

    + it` s scheme for divide cube space on 8 vertex subspace (field)

--- SurfaceReconstruction/SurfaceReconstruction/Engine/Private/Algorithms/CommonStruct.cpp
+++ SurfaceReconstruction/SurfaceReconstruction/Engine/Private/Algorithms/CommonStruct.cpp
@@ -2,3 +2,92 @@
 
 using namespace Engine::Algorithm;
 
+bool SVertexCubeField_0::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if ( x <= _point->GetX() && _point->GetX() < (x + cubeSizeX)
+	  && y <= _point->GetY() && _point->GetY() < (y + cubeSizeY)
+	  && z <= _point->GetZ() &&	_point->GetZ() < (z + cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_1::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x <= _point->GetX() && _point->GetX() < (x + cubeSizeX)
+		&& y >= _point->GetY() && _point->GetY() > (y - cubeSizeY)
+		&& z <= _point->GetZ() && _point->GetZ() < (z + cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_2::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x >= _point->GetX() && _point->GetX() > (x - cubeSizeX)
+		&& y >= _point->GetY() && _point->GetY() > (y - cubeSizeY)
+		&& z <= _point->GetZ() && _point->GetZ() < (z + cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_3::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x >= _point->GetX() && _point->GetX() > (x - cubeSizeX)
+		&& y <= _point->GetY() && _point->GetY() < (y + cubeSizeY)
+		&& z <= _point->GetZ() && _point->GetZ() < (z + cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_4::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x <= _point->GetX() && _point->GetX() < (x + cubeSizeX)
+		&& y <= _point->GetY() && _point->GetY() < (y + cubeSizeY)
+		&& z >= _point->GetZ() && _point->GetZ() > (z - cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_5::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x <= _point->GetX() && _point->GetX() < (x + cubeSizeX)
+		&& y >= _point->GetY() && _point->GetY() > (y - cubeSizeY)
+		&& z >= _point->GetZ() && _point->GetZ() > (z - cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_6::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x >= _point->GetX() && _point->GetX() > (x - cubeSizeX)
+		&& y >= _point->GetY() && _point->GetY() > (y - cubeSizeY)
+		&& z >= _point->GetZ() && _point->GetZ() > (z - cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool SVertexCubeField_7::IsContainPoint(std::shared_ptr<SPoint3D> _point)
+{
+	if (x >= _point->GetX() && _point->GetX() > (x - cubeSizeX)
+		&& y <= _point->GetY() && _point->GetY() < (y + cubeSizeY)
+		&& z >= _point->GetZ() && _point->GetZ() > (z - cubeSizeZ))
+	{
+		return true;
+	}
+	return false;
+}
+
+
--- SurfaceReconstruction/SurfaceReconstruction/Engine/Public/Algorithms/CommonStruct.h
+++ SurfaceReconstruction/SurfaceReconstruction/Engine/Public/Algorithms/CommonStruct.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <memory>
+
 namespace Engine::Algorithm
 {
 	struct SPoint3D
 	{
-		SPoint3D() = delete;
+		SPoint3D() {};
 
 		SPoint3D(double _x, double _y, double _z)
 			: x{_x}
@@ -12,16 +14,170 @@ namespace Engine::Algorithm
 			, z{_z}
 		{};
 
-		double GetX() { return x; };
-		double GetY() { return y; };
-		double GetZ() { return z; };
+		virtual double GetX() const { return x; };
+		virtual double GetY() const { return y; };
+		virtual double GetZ() const { return z; };
 
-	private:
+	protected:
 
 		double x;
 		double y;
 		double z;
 	};
 
+	struct SVertexCubeField : public SPoint3D
+	{
+		SVertexCubeField() {};
+
+		virtual bool IsMesh() const { return bIsMesh; };
+
+		virtual void IncludeInMesh() { bIsMesh = true; };
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) = 0;
+
+	protected:
+
+		bool bIsMesh = false;
+
+		double cubeSizeX;
+		double cubeSizeY;
+		double cubeSizeZ;
+	};
+
+	struct SVertexCubeField_0 : public SVertexCubeField
+	{
+		SVertexCubeField_0() = delete;
+
+		SVertexCubeField_0(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_1 : public SVertexCubeField
+	{
+		SVertexCubeField_1() = delete;
+
+		SVertexCubeField_1(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_2 : public SVertexCubeField
+	{
+		SVertexCubeField_2() = delete;
+
+		SVertexCubeField_2(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_3 : public SVertexCubeField
+	{
+		SVertexCubeField_3() = delete;
+
+		SVertexCubeField_3(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_4 : public SVertexCubeField
+	{
+		SVertexCubeField_4() = delete;
+
+		SVertexCubeField_4(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_5 : public SVertexCubeField
+	{
+		SVertexCubeField_5() = delete;
+
+		SVertexCubeField_5(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_6 : public SVertexCubeField
+	{
+		SVertexCubeField_6() = delete;
+
+		SVertexCubeField_6(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
+
+	struct SVertexCubeField_7 : public SVertexCubeField
+	{
+		SVertexCubeField_7() = delete;
+
+		SVertexCubeField_7(double _x, double _y, double _z, double _cubeSizeX, double _cubeSizeY, double _cubeSizeZ)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+			cubeSizeX = _cubeSizeX;
+			cubeSizeY = _cubeSizeY;
+			cubeSizeZ = _cubeSizeZ;
+		};
+
+		virtual bool IsContainPoint(std::shared_ptr<SPoint3D> _point) override;
+	};
 
 }
